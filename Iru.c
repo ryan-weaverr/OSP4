298367a9f616ed9dbb911f888230d25824c4417e
Ryan <weaverryan575@gmail.com>
2023-04-30 18:56:48 -0400

    complete

--- Iru.c
+++ Iru.c
@@ -1,3 +1,11 @@
+/*
+ Program is set to run with file redirect
+ gcc -g -Wall Iru.c
+ ./a.out < test_1.in | diff - test_1.out
+
+ Can be run with fopen for file input, just change the comment on lines 105 and 112
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -19,7 +27,7 @@ typedef struct
     bool valid;
 } InvertedPageTableEntry;
 
-void print_trace(int virtual_addr, int physical_addr, bool page_fault, PageTableEntry *page_table, InvertedPageTableEntry *inverted_page_table, int *lru_order)
+void print_info(int virtual_addr, int physical_addr, bool page_fault, PageTableEntry *page_table, InvertedPageTableEntry *inverted_page_table, int *lru_order)
 {
     printf(" %02d   %02d ", virtual_addr, physical_addr);
 
@@ -89,43 +97,33 @@ int main(int argc, char *argv[])
 {
     PageTableEntry page_table[VIRTUAL_PAGES] = {0};
     InvertedPageTableEntry inverted_page_table[PHYSICAL_FRAMES] = {0};
-
     int lru_order[PHYSICAL_FRAMES] = {0, 1, 2, 3};
-
-    FILE *trace_file = fopen("test3in.txt", "r");
-    if (!trace_file)
-    {
-        perror("fopen");
-        return 1;
-    }
-
-    printf(" address   page           page table entries          page frames   LRU order\n");
-    printf("virt|phys  fault   | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|   | 0| 1| 2| 3|  LRU...MRU\n");
-    printf("---- ----  -----   |--|--|--|--|--|--|--|--|--|--|   |--|--|--|--|  ---------\n");
-    printf("                                                                     %d,%d,%d,%d\n", lru_order[0], lru_order[1], lru_order[2], lru_order[3]);
-
-    // Process trace file.
     int num_references = 0;
     int num_faults = 0;
     char line[100];
 
-    while (fgets(line, sizeof(line), trace_file))
+    //FILE *input = fopen(argv[1], "r");
+
+    printf("\n address   page           page table entries          page frames   LRU order\n");
+    printf("virt|phys  fault   | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|   | 0| 1| 2| 3|  LRU...MRU\n");
+    printf("---- ----  -----   |--|--|--|--|--|--|--|--|--|--|   |--|--|--|--|  ---------\n");
+    printf("                                                                     %d,%d,%d,%d\n", lru_order[0], lru_order[1], lru_order[2], lru_order[3]);
+
+    //while (fgets(line, sizeof(line), input))
+    while (fgets(line, sizeof(line), stdin))
     {
         int virt_addr = atoi(line);
         int vpn = virt_addr / PAGE_SIZE;
         int offset = virt_addr % PAGE_SIZE;
-        // Look up virtual page number in page table.
+
         PageTableEntry *pte = &page_table[vpn];
 
         if (pte->valid)
         {
-            // Page hit.
+            int i;
             int pfn = pte->pfn;
             int phys_addr = pfn * PAGE_SIZE + offset;
-            
 
-            // Update LRU order.
-            int i;
             for (i = 0; i < PHYSICAL_FRAMES; i++)
             {
                 if (lru_order[i] == pfn)
@@ -134,24 +132,20 @@ int main(int argc, char *argv[])
                 }
             }
 
-            // Shift elements down to fill the gap left by the accessed pfn
             for (; i < PHYSICAL_FRAMES - 1; i++)
             {
                 lru_order[i] = lru_order[i + 1];
             }
 
-            // Insert the accessed pfn at the end
             lru_order[PHYSICAL_FRAMES - 1] = pfn;
-            print_trace(virt_addr, phys_addr, false, page_table, inverted_page_table, lru_order);
+            print_info(virt_addr, phys_addr, false, page_table, inverted_page_table, lru_order);
         }
-        
+
         else
         {
-            // Page fault.
             num_faults++;
 
-            // Find a free frame.
-            int pfn = -1; // Initialize to -1 to indicate no free frame has been found yet
+            int pfn = -1;
             for (int i = 0; i < PHYSICAL_FRAMES; i++)
             {
                 if (!inverted_page_table[i].valid)
@@ -163,11 +157,9 @@ int main(int argc, char *argv[])
 
             if (pfn == -1)
             {
-                // No free frame, evict the least recently used page
                 pfn = lru_order[0];
-
-                // Find the VPN of the page to evict
                 int vpn_to_evict = -1;
+
                 for (int i = 0; i < VIRTUAL_PAGES; i++)
                 {
                     if (page_table[i].valid && page_table[i].pfn == pfn)
@@ -177,37 +169,30 @@ int main(int argc, char *argv[])
                     }
                 }
 
-                // Mark the evicted page as invalid in both page table and inverted page
-                // table
                 page_table[vpn_to_evict].valid = false;
                 inverted_page_table[pfn].valid = false;
             }
 
-            // Update page table and inverted page table with the new page mapping
             pte->vpn = vpn;
             pte->pfn = pfn;
             pte->valid = true;
             inverted_page_table[pfn].vpn = vpn;
             inverted_page_table[pfn].valid = true;
 
-            // Calculate physical address and print trace
             int phys_addr = pfn * PAGE_SIZE + offset;
-            
 
-            // Update LRU order
             for (int i = 0; i < PHYSICAL_FRAMES - 1; i++)
             {
                 lru_order[i] = lru_order[i + 1];
             }
             lru_order[PHYSICAL_FRAMES - 1] = pfn;
-            print_trace(virt_addr, phys_addr, true, page_table, inverted_page_table, lru_order);
+            print_info(virt_addr, phys_addr, true, page_table, inverted_page_table, lru_order);
         }
-
         num_references++;
     }
 
-    printf("\ntrace length = %d addresses, causing %d page faults\n", num_references, num_faults);
-    fclose(trace_file);
+    printf("\ntrace length = %d addresses, causing %d page faults", num_references, num_faults);
+    printf("\n\n");
 
     return 0;
 }
